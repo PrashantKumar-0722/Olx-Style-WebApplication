bab95b71dc48a33d4e357d5d14fb80d23015acfa
Prashant <prashantkumar2531@gmail.com>
2026-02-02 22:51:13 +0530

    frontend added

---
+++ Final Frontend/src/services/api.js
@@ -0,0 +1,18 @@
+// Axios instance with Authorization header taken from localStorage
+
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://frontend-nkg1.onrender.com/api",
+  // baseURL: "https://frontend-nkg1.onrender.com/api",
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
